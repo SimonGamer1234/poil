63652c7349f32df54c356136028612a484e91640
oskarix7 <oskar.vyborny@gmail.com>
2025-04-13 16:16:59 +0200

    Add debug print statement for response URL in run_script function

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def run_script():
         page = 1
         while True:
             response = requests.get(f'https://api.github.com/repos/{OWNER}/{REPO}/actions/variables?page={page}&per_page=100', headers=headers)
+            print(response.url)
             print(response.status_code)
             vgd = response.json()
             if 'variables' not in vgd or not vgd['variables']:
