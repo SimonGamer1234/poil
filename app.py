ce915154fd4e8671bebf0c82587709bf2da63253
Simons <121897649+SimonGamer1234@users.noreply.github.com>
2025-04-13 15:13:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 def run_script():
     # Your script logic here (call function, run something, etc.)
     data = request.get_json()  # Corrected this line
+    Plan = data.get("Plan")
+    Variation = data.get("Variation")
+    Keywords = data.get("Keywords")
+    Message = data.get("Message")
     print("Webhook triggered!", data)
-    return str(data), 200  # Using jsonify to ensure proper JSON response
+    return str(f"Plan: {Plan}. Variation: {Variation}, {Message}, {Keywords}"), 200  # Using jsonify to ensure proper JSON response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
