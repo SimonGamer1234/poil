ff20bbf69355a6c0e905d0aaeb07c4a9397b829e
Simons <121897649+SimonGamer1234@users.noreply.github.com>
2025-04-13 12:10:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def run_script():
     # Your script logic here (call function, run something, etc.)
     data = request.json  # optional
     print("Webhook triggered!", data)
-    return data, 200
+    return jsonify(data), 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
