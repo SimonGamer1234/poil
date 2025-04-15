c1bc92c91506db8c7f608d19cd12db9d96ba8dd3
oskarix7 <oskar.vyborny@gmail.com>
2025-04-15 12:21:00 +0200

    Fix boolean comparison for PostedBefore in webhook function

--- app.py
+++ app.py
@@ -163,7 +163,7 @@ def webhook():
         print(f"V_Names: {V_Names}")
         return V_Names, V_Values, Scheduler_Value, newtable
     Names, Values, Scheduler_Value, IDS = LoadVariables(REPO)
-    if str(PostedBefore) == "True":
+    if str(PostedBefore) == "true":
         def GetGuildIds(IDS):
             ids = [id.strip() for id in str(IDS).split(",")]
             GuildIds = []
@@ -239,7 +239,7 @@ def webhook():
                 print(f" Updating status code: {response.status_code}, Updating text: {response.text}")
         UpdateVariables(Final_Variable, Names, WhichVar, REPO)
         print("Webhook triggered!", data)
-    elif str(PostedBefore) == "False":
+    elif str(PostedBefore) == "false":
         def CreateVariable(Keywords):
             if Variation == "Free Trial":
                 Days = 3
